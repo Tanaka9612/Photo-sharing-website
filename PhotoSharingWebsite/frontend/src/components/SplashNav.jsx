db920e7c5af98a91a4e814ecc75b06158ad15798
Tanaka9612 <85306466+Tanaka9612@users.noreply.github.com>
2026-08-21 14:55:11 +0200

    Majority of Components
    Major components added, currently working on the splash page

---
+++ PhotoSharingWebsite/frontend/src/components/SplashNav.jsx
@@ -0,0 +1,14 @@
+function SplashNav(){
+    return(
+        <nav className="splash-nav-bar">
+            <Link >Features</Link>
+            <Link>Discover</Link>
+            <Link>Community</Link>
+            <Link>About</Link>
+            <Link>Login</Link>
+            <Link>SignUp</Link>
+        </nav>
+    )
+}
+
+export default SplashNav;
---
+++ PhotoSharingWebsite/frontend/src/components/SplashPage.jsx
@@ -0,0 +1,10 @@
+import SplashNav from "./SplashNav";
+
+function SplashPage(){
+    return(
+        <div className="splashpage">
+            <SplashNav/>
+            
+        </div>
+    )
+}
